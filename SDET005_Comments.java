9efd9af19b84abe73c623ba6df151c4cbca974a5
SatyaYellanki <satyayellanki27@gmail.com>
2025-09-01 19:08:08 +0530

    Delete SDET005_Comments.java

--- SDET005_Comments.java
+++
@@ -1,8 +0,0 @@
-package ex_01_introductiontojava;
-
-public class SDET005_Comments {
-	public static void main(String[] args) {
-		// This is a Comment, It will be ignored by JVM/ Compiler
-		System.out.println("Hi");
-	}
-}
