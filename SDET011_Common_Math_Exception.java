20c23f79353c8375cc0cc5894c95c1049022bbcf
SatyaYellanki <satyayellanki27@gmail.com>
2025-09-01 19:10:43 +0530

    Delete SDET011_Common_Math_Exception.java

--- SDET011_Common_Math_Exception.java
+++
@@ -1,8 +0,0 @@
-package ex_01_introductiontojava;
-
-public class SDET011_Common_Math_Exception {
-	public static void main(String[] args) {
-		System.out.println(10 / 0);
-		// java.lang.ArithmeticException
-	}
-}
