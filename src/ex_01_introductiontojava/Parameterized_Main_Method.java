b585c6125ba9a1942eebe115b599d356dc7721b5
Satya <satyayellanki27@gmail.com>
2025-09-02 13:11:42 +0530

    Committing program to Day 1 Java Basics

---
+++ src/ex_01_introductiontojava/Parameterized_Main_Method.java
@@ -0,0 +1,11 @@
+package ex_01_introductiontojava;
+
+public class Parameterized_Main_Method{
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+	
+	public static void main(String args) {
+		
+	}
+}
